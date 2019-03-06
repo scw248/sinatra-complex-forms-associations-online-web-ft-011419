e39d6af5ecf153ed7bb69223aaed8890650de324
Stephen Wright <scw248@gmail.com>
2019-03-06 01:19:53 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,7 +28,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id/edit' do 
     @pet = Pet.find(params[:id])
-    @owners = Owner.all
+    @owner = Owner.all
     erb :"/pets/edit"
   end
   
